79d897df7093cdb66ca3f40d621f49eb6486c6eb
Randy R. Martínez <55677075+randymxd06@users.noreply.github.com>
2021-05-08 20:44:22 -0400

    importé el componente TiposBasicos en App.tsx

--- 01- Reforzamiento/src/App.tsx
+++ 01- Reforzamiento/src/App.tsx
@@ -1,12 +1,20 @@
-//rafc: nos crea una estructura//
+//rafc: nos crea una estructura de nuestro componente//
 
 //Podemos quitar el import porque con las últimas actualizaciones no es necesario tenerlo
 //import React from 'react'
+import { TiposBasicos } from './typescript/TiposBasicos';
 
 export const App = () => {
   return (
     <div className="mt-2">
+
       <h1>Introduccion a TS - React</h1>
+      
+      <hr/>
+
+      {/* Importamos el componente TiposBasicos */}
+      <TiposBasicos />
+
     </div>
   )
 }
