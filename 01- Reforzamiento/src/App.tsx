3f5109667b9369ee52f275fff7a0f2ecdddd78b3
Randy R. Martínez <55677075+randymxd06@users.noreply.github.com>
2021-05-08 20:34:02 -0400

    se quitaron algunas lineas de codigo

--- 01- Reforzamiento/src/App.tsx
+++ 01- Reforzamiento/src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//rafc: nos crea una estructura//
 
-function App() {
+//Podemos quitar el import porque con las últimas actualizaciones no es necesario tenerlo
+//import React from 'react'
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-2">
+      <h1>Introduccion a TS - React</h1>
     </div>
-  );
+  )
 }
 
 export default App;
--- 01- Reforzamiento/src/index.tsx
+++ 01- Reforzamiento/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import './index.css'; ("Este archivo no se ocupa, se puede borrar")
+// import reportWebVitals from './reportWebVitals'; ("Este archivo no se necesita, se puede borrar")
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +11,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// Esto de aqui abajo se puede borrar
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
