0b63ace5a34e3780866b30e07ccc0152309dd172
Randy R. Martínez <55677075+randymxd06@users.noreply.github.com>
2021-05-08 20:43:41 -0400

    Cree un componente y lo llamé TiposBasicos le puse un h3 con un texto

---
+++ 01- Reforzamiento/src/typescript/TiposBasicos.tsx
@@ -0,0 +1,14 @@
+//rafc: para crear la estructura de nuestro componente//
+
+//Quitamos el import de react//
+//import React from 'react'
+
+export const TiposBasicos = () => {
+    return (
+
+        //Trasformamos la etiqueta en un fragmento quitandole la palabra div//
+        <>
+            <h3>Tipos básicos</h3>
+        </>
+    )
+}
