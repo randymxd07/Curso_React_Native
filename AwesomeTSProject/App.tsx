bfb63909058be394e097479a6ef5a433f569d991
Randy R. Martínez <55677075+randymxd06@users.noreply.github.com>
2021-05-12 21:55:57 -0400

    Hice un ejemplo de como crear una app y como depurar la app con mi dispositivo fisico

---
+++ AwesomeTSProject/App.tsx
@@ -0,0 +1,21 @@
+// Importamos React //
+import React from 'react';
+
+// Importamos View, Text de React Native //
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+
+    // En React Native no se usa el div pero si se usa el View //
+    <View>
+      
+      {/* Text es para poner texto en nuestra aplicación */}
+      <Text>Hola desde el Samsung Galaxy S8</Text>
+
+    </View>
+
+  )
+}
+
+export default App;
