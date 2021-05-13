bf6aa6ac64d00d8faf7cdbe29dba341a501ff397
Randy R. Martínez <55677075+randymxd06@users.noreply.github.com>
2021-05-13 00:27:22 -0400

    Hice los StyleSheets para reutilizar estilos cuando yo quiera

--- 02- MiPrimeraApp/src/screens/ContadorScreen.tsx
+++ 02- MiPrimeraApp/src/screens/ContadorScreen.tsx
@@ -1,8 +1,8 @@
 // Importo react, useState //
 import React, { useState } from 'react';
 
-// Importo View, Text, Button //
-import { Text, View, Button } from 'react-native';
+// Importo View, Text, TouchableOpacity, StyleSheet //
+import { Text, TouchableOpacity, View, StyleSheet } from 'react-native';
 
 export const ContadorScreen = () => {
 
@@ -11,36 +11,82 @@ export const ContadorScreen = () => {
 
     return (
 
-        // El flex para que abarque toda la pantalla //
-        // El justifyContent para centrar el contenido de la pantalla //
-        <View style={{flex: 1, justifyContent: 'center'}}>
+        // Hago una vista, le pongo el estilo container //
+        <View style={styles.container}>
 
-            {/* Hago un texto */}
-            {/* El textAlign para centrar el texto */}
-            {/* El fontSize para poner las letras del texto más grandes */}
-            {/* El top -15 para que suba un poco el texto en la pantalla */}
-            <Text style={{textAlign: 'center', fontSize: 40, top: -15}}>
+            {/* Hago un texto, le pongo el estilo title */}
+            <Text style={styles.title}>
 
                 {/* Imprimo el contador del useState */}
                 Contador: {contador}
 
             </Text>
 
-            {/* Hago un botón */}
-            <Button
-                
-                // Título del botón //
-                title="Click"
+            {/* Hago un TouchableOpacity que es como un botón que le puedo aplicar styles */}
+            <TouchableOpacity
 
-                /* Cuando presiono el botón voy a llamar a setContador
-                y incremento en 1 el valor del contador */
-                // Nota: no se puede hacer contador++ ni tampoco contador += 1 //
+                // Cuando presione llamo la funcion setContador y hago que el contador incremente en 1//
                 onPress={() => setContador(contador + 1)}
 
-            />
+            >
+
+                {/* Hago un texto */}
+                {/* <Text>Click on me</Text> */}
+
+                {/* Podemos hacer un botón personalizado, hago una vista */}
+                <View>
+
+                    {/* Hago un texto */}
+                    <Text style={styles.botonIncrementar}>
+                        +1
+                    </Text>
+
+                </View>
+
+            </TouchableOpacity>
 
         </View>
 
     )
 
 }
+
+// Hago una constante para hacer los estilos de mi app//
+// Nota: esto no es CSS //
+const styles = StyleSheet.create({
+
+    // Clase container del const styles //
+    container: {
+
+        // El flex para que abarque toda la pantalla //
+        flex: 1,
+
+        // El justifyContent para centrar el contenido de la pantalla //
+        justifyContent: 'center',
+
+    },
+
+    title: {
+
+        // El textAlign para centrar el texto //
+        textAlign: 'center', 
+
+        // El fontSize para poner las letras del texto más grandes //
+        fontSize: 40,
+
+        // El top -15 para que suba un poco el texto en la pantalla //
+        top: -15,
+
+    },
+
+    botonIncrementar: {
+
+        // Pongo el background gris //
+        backgroundColor: 'grey',
+
+        // El borderRadius es para hacer que el botón este redondeado //
+        borderRadius: 100,
+
+    }
+
+})
