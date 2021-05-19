a8f565dc4c8a746c73f6df2bdd3c8cbacfd98dc6
Randy R. Martínez <55677075+randymxd06@users.noreply.github.com>
2021-05-18 22:02:10 -0400

    use el flex direction

--- 02- MiPrimeraApp/src/screens/FlexScreen.tsx
+++ 02- MiPrimeraApp/src/screens/FlexScreen.tsx
@@ -5,6 +5,8 @@ export const FlexScreen = () => {
 
     return (
 
+        // Por defecto los elementos estan ordenados de manera de columna //
+
         <View style={styles.container}>
             
             <Text style={styles.caja1}>Caja 1</Text>
@@ -24,26 +26,33 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#28C4D9',
+        // flexDirection: 'column', // Por defecto los elementos siempre estan en columnas //
+        flexDirection: 'row',
+        // flexDirection: 'row-reverse', // Es lo mismo que el row pero se invierten las posiciones //
+        // flexDirection: 'column-reverse' // Es lo mismo que el column pero se invierten las posiciones //
     },
 
     // Si el flex es igual en todos entonces se reparte el espacio entre todos //
 
     caja1: {
-        flex: 4, // 40% de la pantalla //
+        // flex: 4, // 40% de la pantalla //
+        // flex: 1,
         borderWidth: 2,
         borderColor: 'white',
         fontSize: 30,
     },
 
     caja2: {
-        flex: 4, // 40% de la pantalla //
+        // flex: 4, // 40% de la pantalla //
+        // flex: 1,
         borderWidth: 2,
         borderColor: 'white',
         fontSize: 30,
     },
 
     caja3: {    
-        flex: 2, // 20% de la pantalla //
+        // flex: 2, // 20% de la pantalla //
+        // flex: 1,
         borderWidth: 2,
         borderColor: 'white',
         fontSize: 30,
