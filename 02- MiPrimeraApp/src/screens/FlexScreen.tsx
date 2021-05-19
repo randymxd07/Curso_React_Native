9a06ca6580f9a2d04135ff3f9ed861eddb8c8186
Randy R. Martínez <55677075+randymxd06@users.noreply.github.com>
2021-05-18 23:08:11 -0400

    aprendi a usar flexWrap, alignSelf, etc

--- 02- MiPrimeraApp/src/screens/FlexScreen.tsx
+++ 02- MiPrimeraApp/src/screens/FlexScreen.tsx
@@ -14,6 +14,61 @@ export const FlexScreen = () => {
             <Text style={styles.caja2}>Caja 2</Text>
 
             <Text style={styles.caja3}>Caja 3</Text>
+            <Text style={styles.caja1}>Caja 1</Text>
+
+            <Text style={styles.caja2}>Caja 2</Text>
+
+            <Text style={styles.caja3}>Caja 3</Text>
+            <Text style={styles.caja1}>Caja 1</Text>
+
+            <Text style={styles.caja2}>Caja 2</Text>
+
+            <Text style={styles.caja3}>Caja 3</Text>
+            <Text style={styles.caja1}>Caja 1</Text>
+
+            <Text style={styles.caja2}>Caja 2</Text>
+
+            <Text style={styles.caja3}>Caja 3</Text>
+            <Text style={styles.caja1}>Caja 1</Text>
+
+            <Text style={styles.caja2}>Caja 2</Text>
+
+            <Text style={styles.caja3}>Caja 3</Text>
+            <Text style={styles.caja1}>Caja 1</Text>
+
+            <Text style={styles.caja2}>Caja 2</Text>
+
+            <Text style={styles.caja3}>Caja 3</Text>
+            <Text style={styles.caja1}>Caja 1</Text>
+
+            <Text style={styles.caja2}>Caja 2</Text>
+
+            <Text style={styles.caja3}>Caja 3</Text>
+            <Text style={styles.caja1}>Caja 1</Text>
+
+            <Text style={styles.caja2}>Caja 2</Text>
+
+            <Text style={styles.caja3}>Caja 3</Text>
+            <Text style={styles.caja1}>Caja 1</Text>
+
+            <Text style={styles.caja2}>Caja 2</Text>
+
+            <Text style={styles.caja3}>Caja 3</Text>
+            <Text style={styles.caja1}>Caja 1</Text>
+
+            <Text style={styles.caja2}>Caja 2</Text>
+
+            <Text style={styles.caja3}>Caja 3</Text>
+            <Text style={styles.caja1}>Caja 1</Text>
+
+            <Text style={styles.caja2}>Caja 2</Text>
+
+            <Text style={styles.caja3}>Caja 3</Text>
+            <Text style={styles.caja1}>Caja 1</Text>
+
+            <Text style={styles.caja2}>Caja 2</Text>
+
+            <Text style={styles.caja3}>Caja 3</Text>
 
         </View>
 
@@ -26,16 +81,39 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#28C4D9',
+
+        /*-----------------
+            flexDirection
+        -------------------*/
         // flexDirection: 'column', // Por defecto los elementos siempre estan en columnas //
         // flexDirection: 'row',
         // flexDirection: 'row-reverse', // Es lo mismo que el row pero se invierten las posiciones //
         // flexDirection: 'column-reverse' // Es lo mismo que el column pero se invierten las posiciones //
+
+        /*------------------
+            justifyContent
+        --------------------*/
         // justifyContent: 'flex-start', // Este es el que siempre esta por defecto //
         // justifyContent: 'flex-end', // Este pone todo el contenido en la parte de abajo //
         // justifyContent: 'center', // Este pone todo el contenido en el centro pero no en el medio de la pantalla, sino en el medio del componente padre //
         // justifyContent: 'space-around', // Distribuye el espacio entre el contenido //
         // justifyContent: 'space-between', // Distribuye el espacio entre el contenido desde el principio hasta el final //
         // justifyContent: 'space-evenly', // Distribuye el espacio entre el contenido, desde el principio hasta el final //
+
+        /*--------------
+            alignItems
+        ----------------*/
+        // alignItems: 'stretch', // La opcion que esta por defecto //
+        // alignItems: 'baseline', // Es lo mismo que el flex-start, toma el espacio minimo //
+        // alignItems: 'flex-start', // Es lo mismo que el flex-start //
+        // alignItems: 'flex-end', // Los alinea al final del elemento //
+        // alignItems: 'center', // Los alinea en el centro //
+
+        /*------------
+            flexWrap
+        --------------*/
+        // flexWrap: 'nowrap', // Es la opcion por defecto //
+        flexWrap: 'wrap', // Cuando hay muchos componentes puedes hacer que todos quepan en pantalla con esto //
 
     },
 
@@ -47,6 +125,18 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         borderColor: 'white',
         fontSize: 30,
+        // alignItems: 'flex-start'
+
+        /*-------------
+            alignSelf
+        ---------------*/
+        // alignSelf: 'flex-start', // Toma el espacio minimo del componente //
+        // alignSelf: 'center', // Toma el espacio minimo del componente en el centro //
+        // alignSelf: 'flex-end', // Toma el espacio minimo del componente en el final //
+        // alignSelf: 'auto', //
+        // alignSelf: 'baseline', //
+        // alignSelf: 'stretch', //
+
     },
 
     caja2: {
@@ -55,6 +145,7 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         borderColor: 'white',
         fontSize: 30,
+        // alignSelf: 'flex-start',
     },
 
     caja3: {    
@@ -63,6 +154,7 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         borderColor: 'white',
         fontSize: 30,
+        // alignSelf: 'flex-end',  
     },
 
 });
