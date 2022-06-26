c9d39e9af180ae320a156268e0818112b2c922e9
Randy Martinez <randym0624@gmail.com>
2022-06-25 21:26:45 -0400

    Renombre la carpeta de la app de calculadora a 04- Calculadora y puse un nuevo proyecto que es donde se hacen los ejemplos de flex, position y box object model

---
+++ 03- FlexPositionBoxObjectModel/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+// import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
+// import { ContadorScreen } from './src/screens/ContadorScreen';
+// import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
+// import { DimensionesScreen } from './src/screens/DimensionesScreen';
+// import { PositionScreen } from './src/screens/PositionScreen';
+// import { FlexScreen } from './src/screens/FlexScreen';
+import { TareaScreen } from './src/screens/TareaScreen';
+
+export const App = () => {
+
+
+  return (
+    <SafeAreaView style={{ 
+      flex: 1,
+      backgroundColor: '#28425B'
+    }}>
+      {/* <HolaMundoScreen /> */}
+      {/* <ContadorScreen /> */}
+      {/* <BoxObjectModelScreen /> */}
+      {/* <DimensionesScreen /> */}
+      {/* <PositionScreen /> */}
+      {/* <FlexScreen /> */}
+      <TareaScreen />
+
+    </SafeAreaView>
+  )
+}
---
+++ 03- FlexPositionBoxObjectModel/src/screens/TareaScreen.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native';
+
+export const TareaScreen = () => {
+    return (
+        <View style={ styles.container }>
+            <View style={ styles.cajaMorada } />
+            <View style={ styles.cajaNaranja } />
+            <View style={ styles.cajaAzul } />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#28425B',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    cajaMorada: {
+        // flex: 2,
+        width: 100,
+        height: 100,
+        borderWidth: 10,
+        borderColor: 'white',
+        backgroundColor: '#5856D6',
+        // alignSelf: 'flex-end',
+        // top: 100
+    },
+    cajaNaranja: {
+        // flex: 2,
+        width: 100,
+        height: 100,
+        // flex: 1, 
+        borderWidth: 10,
+        borderColor: 'white',
+        backgroundColor: '#F0A23B',
+        // alignSelf: 'center'
+        top: 50
+    },
+    cajaAzul: {
+        // flex: 4,
+        width: 100,
+        height: 100,
+        borderWidth: 10,
+        borderColor: 'white',
+        backgroundColor: '#28C4D9',
+        // alignSelf: 'flex-start',
+    },
+});
